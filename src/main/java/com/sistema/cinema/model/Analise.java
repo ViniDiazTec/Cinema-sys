7bdc575c0d1e62b21a48730357f23cfeb4005a09
Vini_Diaz <133820166+ViniDiazTec@users.noreply.github.com>
2024-09-10 20:56:23 -0300

    Criação das classes Model

--- src/main/java/com/sistema/cinema/model/Analise.java
+++ src/main/java/com/sistema/cinema/model/Analise.java
@@ -1,13 +1,51 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.sistema.cinema.model;
 
-/**
- *
- * @author Usuario
- */
 public class Analise {
-    
+
+    private Long id; 
+    private Filme filme; 
+    private String comentario; 
+    private int nota;
+
+    // Construtores
+
+    public Analise(Long id, Filme filme, String comentario, int nota) {
+        this.id = id;
+        this.filme = filme;
+        this.comentario = comentario;
+        this.nota = nota;
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
 }
